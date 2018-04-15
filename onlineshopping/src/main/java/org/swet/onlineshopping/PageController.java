3ce609010956a6ef270a104846602a8e5732db28
swetamber <imminkusavarn@gmail.com>
2018-04-15 11:51:02 +0530

    Completed All the interfaces and theme

--- onlineshopping/src/main/java/org/swet/onlineshopping/PageController.java
+++ onlineshopping/src/main/java/org/swet/onlineshopping/PageController.java
@@ -8,21 +8,32 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value={"/", "/index"})
-	public ModelAndView index()
+	@RequestMapping(value={"/", "/index","/home"})
+	public ModelAndView index(String a)
 	{
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring MVC "); 
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome", true);
 		return mv;   
 	}
 	
 	
-	@RequestMapping(value="/home")
-	public ModelAndView home()
+	@RequestMapping(value="/about")
+	public ModelAndView about()
 	{
-		ModelAndView mv = new ModelAndView("page_backup");
-		mv.addObject("greeting", "Welcome to second Page");
-		return mv;
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;   
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact(String f)
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;   
 	}
 	
 }
